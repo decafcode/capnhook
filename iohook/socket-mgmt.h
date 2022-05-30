df096bd52a7aef82e0974a3e148172c02e20d8f7
Decaf Code <git@decafcode.org>
2022-05-30 10:31:27 +0100

    iohook: Split management and IO ops

--- iohook/socket-mgmt.h
+++ iohook/socket-mgmt.h
@@ -2,15 +2,13 @@
 
 #include "iohook/irp.h"
 
-void iohook_socket_hook_apis(void);
+void iohook_socket_mgmt_hook_apis(void);
 HRESULT iohook_invoke_real_socket(struct irp *irp);
 HRESULT iohook_invoke_real_closesocket(struct irp *irp);
 HRESULT iohook_invoke_real_bind(struct irp *irp);
 HRESULT iohook_invoke_real_connect(struct irp *irp);
 HRESULT iohook_invoke_real_listen(struct irp *irp);
 HRESULT iohook_invoke_real_accept(struct irp *irp);
-HRESULT iohook_invoke_real_recvfrom(struct irp *irp);
-HRESULT iohook_invoke_real_sendto(struct irp *irp);
 HRESULT iohook_invoke_real_ioctlsocket(struct irp *irp);
 HRESULT iohook_invoke_real_getsockname(struct irp *irp);
 HRESULT iohook_invoke_real_getpeername(struct irp *irp);
